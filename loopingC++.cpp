a444242e81144482da595b6619e2c4fa4aa671d1
gigihsatriapradana20 <113432146+gigihsatriapradana20@users.noreply.github.com>
2022-09-13 18:39:10 +0700

    Add files via upload

---
+++ loopingC++.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+int a = 0;
+while (a < 5) {
+    cout << "Belajar Koding di Kelas Algoritma\n"  ;
+    a += 1;
+  }
+    return 0;
+}
---
+++ loopingC.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+
+int i = 0;
+  do{
+  printf( "Belajar Koding di Kelas Algoritma \n");
+    i++;
+  }while(i < 5);
+  
+  return 0;
+}
